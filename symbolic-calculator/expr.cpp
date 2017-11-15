14608c2691c9891f8cdb0141485037a39627848d
ivanmontero <ivanspmontero@gmail.com>
2017-11-14 22:57:28 -0800

    began working on parse method

--- symbolic-calculator/expr.cpp
+++ symbolic-calculator/expr.cpp
@@ -5,21 +5,71 @@
 #include "expr.h"
 #include "symbolicc++.h"
 
-const std::string const Expr::FUNCTION[3] = { "sin", "cos", "tan" };
+const std::string const Expr::FUNCTIONS[3] = { "sin", "cos", "tan" };
+const char const Expr::OPERATORS[5] = { '+','-','*','/','^' };
+const char const Expr::NUMBERS[10] = { '0','1','2','3','4','5','6','7','8','9' };
 
 Expr::Expr(std::string expr) {
-	root = parse(expr);	
+	this->root = parse(expr);	
 
 }
 
 // TODO: Parse into tree
+// Version 1: numbers, parentheses, operators
+// Version 2: functions 
 std::shared_ptr<ExprNode> Expr::parse(std::string expr) {
 	expr.erase(std::remove(expr.begin(), expr.end(), ' '), expr.end()); // Removes whitespace
 	std::stack<std::shared_ptr<ExprNode>> s;
-	s.push(std::make_shared<ExprNode>());
+	std::shared_ptr<ExprNode> root = std::make_shared<ExprNode>();
+	s.push(root);
+	std::shared_ptr<ExprNode> curr = root;
+	for (std::string::iterator it = expr.begin(); it != expr.end(); it++) {
+		print(root);
+		std::cout << std::endl;
+		if (*it == '(') {
+			curr->left = std::make_shared<ExprNode>();
+			s.push(curr);
+			curr = curr->left;
+		} else if (in_array(OPERATORS, *it)) {
+			curr->data = *it;
+			curr->right = std::make_shared<ExprNode>();
+			s.push(curr);
+			curr = curr->right;
+		} else if (in_array(NUMBERS, *it)) {
+			//curr->data = (int)(*it - '0'); // To number
+			curr->data = *it;
+			curr = s.top();
+			s.pop();
+		} else if (*it == ')') {
+			curr = s.top();
+			s.pop();
+		} else {
+			std::cerr << "invalid token encountered" << std::endl;
+		}
+	}
 
+	return root;
+}
 
-	return std::make_shared<ExprNode>("test"); // CHANGE
+void Expr::print() {
+	print(root);
+	std::cout << std::endl;
+}
+
+void Expr::print(std::shared_ptr<ExprNode> root) {
+	if (root) {
+		//TODO: Fix enclose
+		//      * Find out how to see if a "shared_ptr" is null
+		// WORKS!!!
+		bool enclose = root->left.get() != 0 && root->right.get() != 0
+			&& !root->left->data.empty() && !root->right->data.empty();
+
+		if(enclose) std::cout << "(";
+		if (root->left) print(root->left);
+		std::cout << root->data;
+		if (root->right) print(root->right);
+		if (enclose) std::cout << ")";
+	}
 }
 
 template<class T, class E> bool Expr::in_array(T & arr, E & element) { 
@@ -33,5 +83,22 @@ template<class T, class E> bool Expr::in_array(T & arr, E & element) {
 //If the current token is a number, set the root value of the current node to the number and return to the parent.
 //If the current token is a ')', go to the parent of the current node.
 
-//
-//int main() { Expr e("hx"); }
+//testing purposes
+int main() { 
+
+	// Trouble case
+	Expr e("(2 + (2 * 2) ^ 2)"); 
+	e.print();
+
+
+	//std::cout << (int)('5' - '0') << std::endl;
+
+	/*Symbolic x("x");
+	Symbolic f = x + 1;
+	f = x * f;
+	f = ((f) ^ 2) * x;*/
+
+
+	std::cin.get();
+
+}
--- symbolic-calculator/expr.h
+++ symbolic-calculator/expr.h
@@ -9,20 +9,26 @@ struct ExprNode {
 	std::shared_ptr<ExprNode> left;
 	std::shared_ptr<ExprNode> right;
 
-	ExprNode() : data(), left(), right() {}
-	ExprNode(std::string data) : data(data), left(), right() {}
+	ExprNode() : data() {}
+	ExprNode(std::string data) : data(data) {}
 };
 
 class Expr {
 public:
 	Expr(std::string expr);
+
+	void print();
 	
 private:
-	std::shared_ptr<ExprNode> root;
 	std::shared_ptr<Symbolic> expr;
+	std::shared_ptr<ExprNode> root;
 	std::shared_ptr<ExprNode> parse(std::string expr);
+
+	void print(std::shared_ptr<ExprNode> root);
 
 	template<class T, class E> bool in_array(T & arr, E & element);
 public:
-	static const std::string const FUNCTION[3];
+	static const std::string const FUNCTIONS[3];
+	static const char const OPERATORS[5];
+	static const char const NUMBERS[10];
 };
