c71f5d4d9683227524e675d045de83c72007ae84
ivanmontero <ivanspmontero@gmail.com>
2017-11-17 23:23:31 -0800

    created static methods

--- symbolic-calculator/expr.h
+++ symbolic-calculator/expr.h
@@ -7,16 +7,14 @@ class Symbolic;
 class Expr {
 public:
 	Expr(std::string expr);
-
-	void print();
 	
 private:
 	std::shared_ptr<Symbolic> expr;
-	std::shared_ptr<Symbolic> parse(std::string expr);
-
-	template<class T, class E> bool in_array(T & arr, E & element);
-	template<class T, class E> int index_of(T & arr, E & element);
 public:
+	static std::shared_ptr<Symbolic> parse(std::string expr);
+	template<class T, class E> static bool in_array(T & arr, E & element);
+	template<class T, class E> static int index_of(T & arr, E & element);
+
 	static const std::string const FUNCTIONS[4];
 	static const std::string const SPECIAL_NUMBERS[2];
 	static const char const PARENTHESES[2];
